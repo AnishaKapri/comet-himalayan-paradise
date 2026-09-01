572c9643be57ebdcda1f0c270cf8d5821cefa87e
Mohit <mohitdhami008@email.com>
2026-09-01 09:15:29 +0530

    update in purpose Driven Page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const moreLinks = [
   { href: "/contact", label: "Second Home" },
   { href: "/#facilities", label: "CHP Facilities" },
   { href: "/contact", label: "Business & Investment" },
-  { href: "/#purpose-driven-space", label: "Purpose Driven Space" },
+  { href: "/purpose-driven-space", label: "Purpose Driven Space" },
   { href: "/chp-enclave", label: "CHP Enclave" },
 ];
 
